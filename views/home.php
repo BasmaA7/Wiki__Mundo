c2170371140b067e905f3e102e34e14df89ad783
Basma <basmaelandalosy5@gmail.com>
2024-01-11 01:24:26 +0100

    modification page home

--- views/home.php
+++ views/home.php
@@ -5,15 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
 
-  <title>Document</title>
+  <title>Wiki__Mundo</title>
   <link rel="stylesheet" href="../public/style/dist/output.css">
 </head>
-<body>
-
+<body class="bg-gray-100">
 <?php include "includes/nav.php";?>
+<section class=" container px-5 py-24 mx-auto bg-center" style="background-image: url('../public/assets/images/heropic.png')" >
 
-
-<section class=" py-3" style="background-image:url(../public/assets/images/p-hero.png)" >
 <div class="py-8 px-4  lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16 " >
         <div class="flex flex-col justify-center">
             <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-5xl dark:text-white">" let's Exploring Together  with Wiki_Mundo!"</h1>
@@ -36,65 +34,104 @@
 
 <!--  les plus populaire -->
 
- <section >
- <span class="flex  justify-center text-3xl my-8">Les Dernières Articles </span>
-  <div class="flex space-x-4 my-8">  
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="../public/assets/images/artpic.png" alt="" />
-    </a>
-    <div class="p-3">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Developement</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
-    </div>
-</div>
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="../public/assets/images/artpic.png" alt="" />
-    </a>
-    <div class="p-3">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Musique</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
-    </div>
-</div>
+ <section class=" " >
+  <div class="container px-5 py-24 mx-auto">
+ <div class="text-center mb-12">
+                <h5 class="text-base md:text-lg text-indigo-700 mb-1">See Our Recent News</h5>
+                <h1 class="text-4xl md:text-6xl text-gray-700 font-semibold">Tailwind Css Responsive Cards</h1>
+            </div>  
+            <div class="flex space-x-4 my-8">  
+
+
 
 
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="../public/assets/images/artpic.png" alt="" />
-    </a>
-    <div class="p-3">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Art</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+                            src="../public/assets/images/music.png" alt="blog">
+                        <div class="p-6 hover:bg-red-700 hover:text-white transition duration-300 ease-in">
+                            <h2 class="text-base font-medium text-indigo-300 mb-1">October 29,
+                                2021</h2>
+                            <h1 class="text-2xl font-semibold mb-3">Cities are crowded</h1>
+                            <p class="leading-relaxed mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit
+                                fuga! Dolores quisquam eius cum accusamus?</p>
+                         
+                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
         </a>
+                        </div>
+                   
+                </div>
+</div>
+
+
+
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+                            src="../public/assets/images/music.png" alt="blog">
+                        <div class="p-6 hover:bg-red-700 hover:text-white transition duration-300 ease-in">
+                            <h2 class="text-base font-medium text-indigo-300 mb-1">October 29,
+                                2021</h2>
+                            <h1 class="text-2xl font-semibold mb-3">Cities are crowded</h1>
+                            <p class="leading-relaxed mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit
+                                fuga! Dolores quisquam eius cum accusamus?</p>
+                         
+                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+                        </div>
+                   
+                </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+                            src="../public/assets/images/music.png" alt="blog">
+                        <div class="p-6 hover:bg-red-700 hover:text-white transition duration-300 ease-in">
+                            <h2 class="text-base font-medium text-indigo-300 mb-1">October 29,
+                                2021</h2>
+                            <h1 class="text-2xl font-semibold mb-3">Cities are crowded</h1>
+                            <p class="leading-relaxed mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit
+                                fuga! Dolores quisquam eius cum accusamus?</p>
+                         
+
+                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+                              
+                        </div>
+
+                        
     </div>
 </div>
 </div>
 </section>
-<section >
+<!-- <section >
  <span class="flex  justify-center text-3xl my-8">Les Autres Articles </span>
   <div class="flex space-x-4 my-8">  
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -151,7 +188,7 @@
     </div>
 </div>
 </div>
-</section>
+</section> -->
  <!--  footer section -->
 
  <?php include "includes/footer.php";?>
@@ -159,10 +196,13 @@
  
 
 
-<!--  les plus populaire -->
+
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 
 <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
+
+
+
