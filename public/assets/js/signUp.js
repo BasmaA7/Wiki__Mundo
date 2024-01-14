c969e4b0d103310a2696c181ffb28698ee1ceedf
Basma <basmaelandalosy5@gmail.com>
2024-01-14 01:45:08 +0100

    validation js signup done

---
+++ public/assets/js/signUp.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let inputName = e.target.elements.name.value;
+  let inputEmail = e.target.elements.email.value;
+  let inputPassword = e.target.elements.password.value;
+
+  const nameError = document.querySelector("#name-error");
+  const emailError = document.querySelector("#email-error");
+  const passwordError = document.querySelector("#password-error");
+
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/i;
+  const nameRegex = /^[A-Za-z]+$/;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  // Réinitialiser les messages d'erreur
+  nameError.classList.add("hidden");
+  emailError.classList.add("hidden");
+  passwordError.classList.add("hidden");
+
+  // Valider les champs et afficher les erreurs si nécessaire
+  if (!nameRegex.test(inputName)) {
+    nameError.classList.remove("hidden");
+  }
+
+  if (!emailRegex.test(inputEmail)) {
+    emailError.classList.remove("hidden");
+  }
+
+  if (!passwordRegex.test(inputPassword)) {
+    passwordError.classList.remove("hidden");
+  }
+});
--- views/SignUp.php
+++ views/SignUp.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,51 +8,110 @@
 
   <title>Document</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+ 
+
 
 </head>
+
 <body>
-<section class="bg-gray-100 dark:bg-gray-900">
-    <div class="py-8 px-4  lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16 " style="background-image:url(../public/assets/images/signPic.png)">
-        <div class="flex flex-col justify-center">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-            <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-            <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more about our app 
-                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-        <div>
-            <div class=" lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white  rounded-lg shadow-xl dark:bg-gray-800 ">
-                
-                <h1 class="text-3xl font-semibold mb-6  text-gray-900 text-center">Sign <span class="text-orange-500"> Up</span></h1>
-      <h1 class="text-sm font-semibold mb-6 text-gray-500 text-center">Join to Our Community with all time access and free </h1> 
-      <form  method="POST" action="/Wiki__Mundo/public/index.php?action=addusers" class="space-y-4 ">
-        <div>
-          <label for="username" class="block text-sm font-medium  text-gray-900">Username</label>
-          <input type="text"  name="name" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
-        </div>
-        <div>
-          <label for="email" class="block text-sm font-medium  text-gray-900">Email</label>
-          <input type="email"  name="email" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
-        </div>
-        <div>
-          <label for="password" class="block text-sm font-medium  text-gray-900">Password</label>
-          <input type="password" id="password" name="password" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
-        </div>
-        
-          <button type="submit" name="signup" class="w-full bg-black  text-gray-900p-2 rounded-md hover:bg-gray-800  focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign Up</button>
-        </div>
-      </form>
-                
-                
+  <section class="bg-gray-100 dark:bg-gray-900">
+    <div class="py-8 px-4  lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16 "
+      style="background-image:url(../public/assets/images/signPic.png)">
+      <div class="flex flex-col justify-center">
+        <h1
+          class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          We invest in the world’s potential</h1>
+        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on
+          markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <a href="#"
+          class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read
+          more about our app
+          <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
+        </a>
+      </div>
+      <div>
+        <div class=" lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white  rounded-lg shadow-xl dark:bg-gray-800 ">
+
+          <h1 class="text-3xl font-semibold mb-6  text-gray-900 text-center">Sign <span class="text-orange-500">
+              Up</span></h1>
+          <h1 class="text-sm font-semibold mb-6 text-gray-500 text-center">Join to Our Community with all time access
+            and free </h1>
+          <!-- <form id="form" method="POST" action="/Wiki__Mundo/public/index.php?action=addusers" class="space-y-4 ">
+            <div>
+              <label for="name" class="block text-sm font-medium  text-gray-900">Username</label>
+              <input id="name" type="text" name="name"
+                class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+              <span class="text-red-600 hidden " id="name-error">no valid</span>
+
             </div>
+            <div>
+              <label for="email" class="block text-sm font-medium  text-gray-900">Email</label>
+              <input id="email" type="email" name="email"
+                class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+              <span class="text-red-600 hidden " id="email-error">no valid</span>
+
+            </div>
+            <div>
+              <label for="password" class="block text-sm font-medium  text-gray-900">Password</label>
+              <input id="password" type="password" id="password" name="password"
+                class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+              <span class="text-red-600 hidden " id="password-error">no valid</span>
+
+            </div>
+
+            <button type="submit" id="submit" name="signup"
+              class="w-full bg-black  text-gray-900p-2 rounded-md hover:bg-gray-800  focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign
+              Up</button>
         </div>
+        </form> -->
+
+        <form id="form" method="POST" action="/Wiki__Mundo/public/index.php?action=addusers" class="space-y-4 ">
+    <div>
+      <label for="name" class="block text-sm font-medium  text-gray-900">Username</label>
+      <input id="name" type="text" name="name"
+        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+        <div class='bg-red-100  border-red-600 mt-3  '>
+      <span class="text-red-600 hidden " id="name-error">Invalid username</span>
+      </div>
     </div>
-</section>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+    <div>
+      <label for="email" class="block text-sm font-medium  text-gray-900">Email</label>
+      <input id="email" type="email" name="email"
+        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+        <div class='bg-red-100  border-red-600 mt-3  '>
+      <span class="text-red-600 hidden " id="email-error">Invalid email</span>
+      </div>
+    </div>
 
-<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    <div>
+      <label for="password" class="block text-sm font-medium  text-gray-900">Password</label>
+      <input id="password" type="password" name="password"
+        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+        <div class='bg-red-100  border-red-600 mt-3  '>
+      <span class="text-red-600 hidden " id="password-error">Invalid password</span>
+      </div>
+    </div>
+
+    <button type="submit" id="submit" name="signup"
+      class="w-full bg-black  text-gray-900p-2 rounded-md hover:bg-gray-800  focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign Up</button>
+  </form>
+
+
+
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+
+  <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+  <script src="../public/assets/js/signUp.js"></script>
 </body>
+
 </html>
